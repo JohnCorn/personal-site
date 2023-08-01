4051072b7c8e9cb18917a837c991871e6e948ccd
John Corn <jcornacchioli@gmail.com>
2023-08-01 17:32:34 -0400

    stylized card

--- src/components/WorkCard.jsx
+++ src/components/WorkCard.jsx
@@ -13,10 +13,10 @@ export default function WorkCard(
 }) 
 {
   return (
-    <div className='shadow-xl shadow-slate-400/50 bg-gray-50 container rounded-2xl flex justify-center items-center mx-auto'>
+    <div className='text-gray-50 bg-[#18191C] container rounded-[32px] flex justify-center items-center mx-auto'>
 
-        <div className='p-4'>
-        <div className='h-full rounded-xl items-center justify-center overflow-hidden'>
+        <div className='p-4 mt-2'>
+        <div className='h-full aspect-square rounded-3xl items-center justify-center overflow-hidden'>
             <img  
             src={image} 
             alt={name}
@@ -24,19 +24,20 @@ export default function WorkCard(
             />
         </div>
 
-        <div className='h-full w-full text-center justify-center'>
-            <h2>{role}</h2>
-            <h1 className='text-2xl font-bold tracking-wider'>
+        <div className='h-full w-full justify-center mt-6 px-5'>
+            <h1 className='text-2xl font-semibold tracking-wider'>
                 {name}
             </h1>
-            <p>{description}</p>
+            <p className='text-gray-400 font-semibold mt-2'>{description}</p>
 
-            <div >
-                <a href="/">
-                    <button className='text-center rounded-md px-4 py-1 m-2 bg-gray-500 font-bold text-lg'>Demo</button>
+            <div className='flex justify-around mt-8'>
+                <a 
+                href="/">
+                    <button className='text-center rounded-2xl px-8 py-4 m-2 bg-[#1649FF] text-md'>demo</button>
                 </a>
-                <a href="/">
-                    <button className='text-center rounded-md px-4 py-1 m-2 bg-gray-500 font-bold text-lg'>Code</button>
+                <a 
+                href="/">
+                    <button className='text-center rounded-2xl px-8 py-4 m-2 bg-[#1649FF] text-md'>code</button>
                 </a>
             </div>
             
